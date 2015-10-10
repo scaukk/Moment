55fc986f96a7739c3887f147840cdedfd7eb22b0
momo <294666880@qq.com>
2015-10-10 09:35:35 +0800

    生成Tools模块初始文件

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
-// 绑定Admin模块到当前入口文件
-define('BIND_MODULE','Admin');
-define('BUILD_CONTROLLER_LIST','Base,Index,User,Manager');
-define('BUILD_MODEL_LIST','Base,User');
+// 绑定Tools模块到当前入口文件
+define('BIND_MODULE','Tools');
+define('BUILD_CONTROLLER_LIST','Verify');
+// define('BUILD_MODEL_LIST','Base,User');
 
 // 定义应用目录
 define('APP_PATH','./Application/');
