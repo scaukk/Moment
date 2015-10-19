0aa3cce7101bf15e78be8bbdfa77dd5d138f7f89
momo <294666880@qq.com>
2015-10-19 19:11:41 +0800

    增加 项目公共函数
    1、p()函数，方便打印数组
    
    2、datetime()函数，得到数据库datetime格式的日期时间字符串

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -9,9 +9,16 @@
  * @param  array $arr 数组
  */
 function p($arr){
-	echo "<pre>";
-	print_r($arr);
-	echo "</pre>";
+    echo "<pre>";
+    print_r($arr);
+    echo "</pre>";
 }
 
-?>
+/**
+ * 当前日期时间
+ * 格式如：1991-01-01 14:08:27
+ * @return string
+ */
+function datetime() {
+    return date('Y-m-d H:i:s', NOW_TIME);
+}
